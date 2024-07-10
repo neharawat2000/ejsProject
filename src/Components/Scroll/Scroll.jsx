1bbafde4b7edf5b75896e45621e3b439e9271c6a
Neha Rawat <135629831+neharawat2000@users.noreply.github.com>
2024-07-10 20:17:53 +0530

    Add files via upload

--- src/Components/Scroll/Scroll.jsx
+++ src/Components/Scroll/Scroll.jsx
@@ -4,8 +4,8 @@ import proposal from "../../resources/proposal.png";
 import { Link } from "react-router-dom";
 import time from "../../resources/time.png";
 import maintain from "../../resources/maintain.png";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
 
 export default function Scroll(){
     const frameData= [
@@ -21,115 +21,63 @@ export default function Scroll(){
             parah:"Immediate assistance around the clock, ensuring your property is always in expert hands."
         }
     ];
+    const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()] )
+
     return(
         <>
-            <Carousel showThumbs={false} showIndicators={false} infiniteLoop={true} autoPlay={true}
-            className='boxes1'
-            >
-                <div className="box">
-                    <h2>Apply Online</h2>
-                    <div className="section">
-                        <p>
-                            Get started with our quick and easy application portal for seamless property management services.
-                        </p>
-                        <Link to="/get-started">
-                        <button className="letWorkBtn">
-                            LETS WORK
-                        </button>
-                        </Link>
-                    </div>
-                </div>
-                <div className="box proposal">
-                    <img src={proposal} alt="proposal-img" />
-                    <h2>Review Proposals</h2>
-                    <button className="letWorkBtn">
-                        VIEW MORE
-                    </button>
-                </div>
-                <div className="box">
-                    <h2>Get Started</h2>
-                    <div className="section">
-                        <p>
-                            Choose your package and begin experiencing our
-                            premium property management services.
-                        </p>
-                        <Link to="/get-started">
-                            <button className="letWorkBtn"> 
-                                LETS WORK
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-                {
-                    frameData.map((data)=>{
-                        return(
-                            <div className="frame">
-                                <img src={data.img} alt="24hr-image"/>
-                                <div className="section">
-                                    <h6>
-                                        {data.title}
-                                    </h6>
-                                    <p>{data.parah}</p>
-                                </div>
+            <div className="embla" ref={emblaRef}>
+                <div className='boxes'>
+                        <div className="box embla__slide">
+                            <h2>Apply Online</h2>
+                            <div className="section">
+                                <p>
+                                    Get started with our quick and easy application portal for seamless property management services.
+                                </p>
+                                <Link to="/get-started">
+                                <button className="letWorkBtn">
+                                    LETS WORK
+                                </button>
+                                </Link>
                             </div>
-                        );
-                    })
-                }
-            </Carousel>
-            <Carousel showThumbs={false} showIndicators={false} infiniteLoop={true} autoPlay={true} centerMode
-            centerSlidePercentage={50}
-            className='boxes'
-            >
-                <div className="box">
-                    <h2>Apply Online</h2>
-                    <div className="section">
-                        <p>
-                            Get started with our quick and easy application portal for seamless property management services.
-                        </p>
-                        <Link to="/get-started">
-                        <button className="letWorkBtn">
-                            LETS WORK
-                        </button>
-                        </Link>
-                    </div>
-                </div>
-                <div className="box proposal">
-                    <img src={proposal} alt="proposal-img" />
-                    <h2>Review Proposals</h2>
-                    <button className="letWorkBtn">
-                        VIEW MORE
-                    </button>
-                </div>
-                <div className="box">
-                    <h2>Get Started</h2>
-                    <div className="section">
-                        <p>
-                            Choose your package and begin experiencing our
-                            premium property management services.
-                        </p>
-                        <Link to="/get-started">
-                            <button className="letWorkBtn"> 
-                                LETS WORK
+                        </div>
+                        <div className="box proposal embla__slide">
+                            <img src={proposal} alt="proposal-img" />
+                            <h2>Review Proposals</h2>
+                            <button className="letWorkBtn">
+                                VIEW MORE
                             </button>
-                        </Link>
-                    </div>
-                </div>
-                {
-                    frameData.map((data)=>{
-                        return(
-                            <div className="frame">
-                                <img src={data.img} alt="24hr-image"/>
-                                <div className="section">
-                                    <h6>
-                                        {data.title}
-                                    </h6>
-                                    <p>{data.parah}</p>
-                                </div>
+                        </div>
+                        <div className="box embla__slide">
+                            <h2>Get Started</h2>
+                            <div className="section">
+                                <p>
+                                    Choose your package and begin experiencing our
+                                    premium property management services.
+                                </p>
+                                <Link to="/get-started">
+                                    <button className="letWorkBtn"> 
+                                        LETS WORK
+                                    </button>
+                                </Link>
                             </div>
-                        );
-                    })
-                }
-            </Carousel>
+                        </div>
+                        {
+                            frameData.map((data)=>{
+                                return(
+                                    <div className="frame embla__slide">
+                                        <img src={data.img} alt="24hr-image"/>
+                                        <div className="section">
+                                            <h6>
+                                                {data.title}
+                                            </h6>
+                                            <p>{data.parah}</p>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
+                </div>
+            </div> 
         </>
         
     );
